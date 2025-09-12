b2b710819f79bf5ba2328e82aad84d0b7f3f17d3
Kalindu Tharanga <71347341+kalinduTh@users.noreply.github.com>
2025-09-12 11:07:51 +0530

    fix: git style fix

--- src/Layouts/Header.jsx
+++ src/Layouts/Header.jsx
@@ -3,7 +3,7 @@ import LogoutButton from '../Components/LogoutButton'
 
 function Header() {
   return (
-    <div className='w-[100%] flex justify-between items-center relative px-5 justify-center bg-white h-[10vh]'>
+    <div className='w-[100%] flex items-center relative px-5 justify-center bg-white h-[10vh]'>
       <div className='flex mx-auto flex-col items-end'>
         <h1 className='text-gray-800 text-xl font-semibold leading-4'>UK care Home</h1>
         <p className='text-blue-600'>Admin</p>
